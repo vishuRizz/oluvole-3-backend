c08d527590cf4ca485af7301ed1c0edc465b2480
zekud <zekudk@gmail.com>
2024-11-30 22:08:38 +0300

    Refactor date formatting in payment service

--- services/payment.service.js
+++ services/payment.service.js
@@ -64,11 +64,11 @@ const create = asyncErrorHandler(async (req, res) => {
         roomDetails?.selectedRooms?.map((room) => ` ${room.title}`) ||
         "Day Pass",
       checkIn: roomDetails?.visitDate
-        ? formatDate(roomDetails?.visitDate)
-        : roomDetails?.startDate,
+        ? `${formatDate(roomDetails?.visitDate)}, 2pm`
+        : `${roomDetails?.startDate}, 12noon`,
       checkOut: roomDetails?.endDate
-        ? formatDate(roomDetails?.endDate)
-        : roomDetails?.startDate,
+        ? `${formatDate(roomDetails?.endDate)}, 11am`
+        : `${roomDetails?.startDate}, 6pm`,
       numberOfGuests: roomDetails?.visitDate
         ? `${
             roomDetails?.selectedRooms?.[0]?.guestCount?.adults ?? 0
