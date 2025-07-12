d5fa8beffde81b1422808ab47e9a74cacb5a9dea
Ali Zaid <alizaidkhoso@gmail.com>
2025-07-12 19:30:22 +0500

    squad issue is fixed now

--- routes/daypass.router.js
+++ routes/daypass.router.js
@@ -1,18 +1,13 @@
 const { create, getAll, getSingle } = require('../services/daypass.service')
-
-const router = require('express').Router()
-
+const express = require('express')
+const router = express.Router()
+const { initiateDaypassBooking } = require('../services/daypass.service')
 
 router.post("/create",create)
 router.get("/get/all",getAll)
 router.get("/get/single/:id",getSingle)
 
-
-
-
-
-
-
-
+// Initiate booking before payment
+router.post('/bookings/initiate', initiateDaypassBooking)
 
 module.exports = router
